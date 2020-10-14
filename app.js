a1b63a38f2a66ab6f130cae3e49eb82583d25967
rf-spuds <rfspuds@gmail.com>
2020-10-14 12:39:00 -0400

    dummy requests for geocoding, weather

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const request = require("postman-request")
+
+const map_url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Brooklyn.json?access_token=" + process.env.mapbox_key
+
+const weather_url = "http://api.weatherstack.com/current?access_key=" + process.env.weather_key + "&query=" + process.env.my_zip + "&units=f"
+
+// request ({url: weather_url, json: true}, (error, response)=>{
+//     // console.log(response.body.current)
+//     console.log("It is currently " + response.body.current.temperature + " degrees out. The real feel is " + response.body.current.feelslike + " degrees." );
+// })
+
+// request ({url: map_url, json: true}, (error, response)=>{
+//     // console.log(response.body.current)
+//    console.log("Coords for " + response.body.features[0].place_name);
+//    console.log("lat= " + response.body.features[0].center[1]);
+//    console.log("lon= " + response.body.features[0].center[0]);
+// })
