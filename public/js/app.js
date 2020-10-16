29d8083d4d02b3f6097093e7491777752c60bb8d
rf-spuds <rfspuds@gmail.com>
2020-10-16 11:28:52 -0400

    Removed pointless routes and code

--- public/js/app.js
+++ public/js/app.js
@@ -1,46 +1,25 @@
-console.log("client side js is loaded, sucka");
-
-// fetch("http://puzzle.mead.io/puzzle").then((response) => {
-//   response.json().then((data) => {
-//     console.log(data);
-//   });
-// });
-
-// fetch("http://127.0.0.1:3000/weather?address=burlington").then((response) => {
-//   response.json().then((data) => {
-//     if (data.error) {
-//       console.log(data.error);
-//     } else {
-//       console.log(data.location);
-//     }
-//   });
-// });
 
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
-const messageOne= document.querySelector('#messageOne')
+const messageOne = document.querySelector('#messageOne')
 const messageTwo = document.querySelector("#messageTwo")
 
-weatherForm.addEventListener('submit', (e)=>{
-    e.preventDefault()
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
 
-    const location  = search.value
+  const location = search.value
 
-    messageOne.textContent ="Loading..."
+  messageOne.textContent = "Loading..."
 
-    fetch("http://127.0.0.1:3000/weather?address=" + location).then((response) => {
-        response.json().then((data) => {
-          if (data.error) {
-            messageOne.textContent=data.error;
-          } else {
-            messageOne.textContent=data.location;
-            messageTwo.textContent=data.forecastData
-          }
-        });
-      });
-      
-
-
-
+  fetch("http://127.0.0.1:3000/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecastData
+      }
+    });
+  });
 })
--- src/app.js
+++ src/app.js
@@ -1,110 +1,65 @@
-
 require("dotenv").config();
 
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
 
-const geocode = require("./utils/geocode")
-const forecast = require("./utils/forecast")
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
 const app = express();
 
-//  paths 
+//  paths
 const publicDirectory = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials")
+const partialsPath = path.join(__dirname, "../templates/partials");
 
 // templating
 app.set("view engine", "hbs");
-app.set('views', viewsPath);
-hbs.registerPartials(partialsPath)
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicDirectory));
 
-// Routes 
+// Routes
 
 app.get("", (req, res) => {
   res.render("weather", {
-    title: "Weather"
-  });
-});
-
-app.get("/about", (req, res) => {
-  res.render("about", {
-      title: "about",
-    question: "Do you?",
-    sum: 2 + 2,
-  });
-});
-
-app.get("/help", (req, res) => {
-  res.render("help", {
-      title: "help",
-    helpMessage: "Abandon all hope",
+    title: "Weather",
   });
 });
 
 app.get("/weather", (req, res) => {
-  
-  if(!req.query.address){
+  if (!req.query.address) {
     return res.send({
-      error:"I need an address, ya fool"
-    })
+      error: "I need an address",
+    });
   }
-    geocode(req.query.address, (error, {lat,lon,location}={})=>{
-      if(error){
-        return res.send({error})
+  geocode(req.query.address, (error, { lat, lon, location } = {}) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    forecast(lat, lon, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
       }
-
-      forecast(lat, lon, (error, forecastData)=>{
-        if(error){
-          return res.send({ error })
-        }
-        res.send({
-          forecastData: forecastData,
-          location,
-          address: req.query.address
-        })
-      })
-
-    })
-
-
-
+      res.send({
+        forecastData: forecastData,
+        location,
+        address: req.query.address,
+      });
+    });
   });
-
-app.get("/products", (req,res) =>{
-  if(!req.query.search){
-    return res.send({
-      error: "I need a search"
-    })
-  }
-  res.send({
-    products:[]
-  })
-})
-
-
-
-app.get("/help/*", (req, res)=>{
-    res.render("404", {
-        message: "No Help article with that name!",
-        img:"https://i.kym-cdn.com/photos/images/original/001/809/295/3d5.jpg"
-    })
-})
-
-
+});
 
 // 404
-app.get('*', (req, res)=>{
-    res.render("404",{
-        message:"Looks like you're lost, bud",
-        img:"https://media1.tenor.com/images/7b4fd20fe96029eedd82506f3a9f9491/tenor.gif?itemid=5450017"
-    })
-
-})
-
+app.get("*", (req, res) => {
+  res.render("404", {
+    message: "Looks like you're lost, bud",
+    img: "https://i.kym-cdn.com/photos/images/original/001/809/295/3d5.jpg",
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
